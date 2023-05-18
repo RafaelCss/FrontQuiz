c148d6e6c00fade4bf56b06d2d2c84e85b07a43a
devspacerj <rf2014souza@gmail.com>
2023-05-17 22:45:08 -0300

    17-05 criando serviços de requisicão

--- src/Func/configAxios.ts
+++ src/Func/configAxios.ts
@@ -5,6 +5,12 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: process.env.API_URL_CONNECT,
   timeout: 1000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  paramsSerializer: {
+    dots: true,
+  },
 });
 
-// export default api;
+export default api;
