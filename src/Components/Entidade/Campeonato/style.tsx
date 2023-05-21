e0e92f13f3c52138aa7a723a33b46d4735f1cd88
devspacerj <rf2014souza@gmail.com>
2023-05-21 20:38:30 -0300

    21-05 criada a pagina da tabela do campeonato

---
+++ src/Components/Entidade/Campeonato/style.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const ContainerTabela = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  width: 100%;
+  margin-top: 5px;
+  justify-content: center;
+  background-color: black;
+`;
---
+++ src/pages/Campeonato/index.tsx
@@ -0,0 +1,5 @@
+import TabelaCampeonato from '@/src/Components/Entidade/Campeonato';
+
+export default function PageCampeonato() {
+  return <TabelaCampeonato />;
+}
