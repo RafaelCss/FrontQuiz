f97ef571e20a7a518b928f0d8c805960af9e2295
devspacerj <rf2014souza@gmail.com>
2023-06-15 22:40:47 -0300

    feat-14-06 ajustes na inportação de arquivos

--- src/Func/configAxios.ts
+++ src/Func/configAxios.ts
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_URL_API_URL_CONNECT,
+  headers: {
+    'Content-Type': 'application/json',
+  },
   paramsSerializer: {
     dots: true,
   },
