ee5d7f38c017194e34970d0443488211c4b6fdb6
RafaelCss <rf2014souza@gmail.com>
2023-08-11 23:17:34 -0300

    ajuste no projeto

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    session({ session, token }) {
+      return session;
+    },
+  },
+};
+
+export default NextAuth(authOptions);
