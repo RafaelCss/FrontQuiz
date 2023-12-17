854bc73c1c452ef196e7f194a38f3cb9528d9a55
RafaelCss <rf2014souza@gmail.com>
2023-12-16 21:20:47 -0300

    bugfix/16-12 ajuste no formulário e redirecionamento de login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,43 +3,43 @@ import NextAuth, { NextAuthOptions, Session } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import servico from '@/Func/servicos/usuarioServico';
 
-async function refreshAccessToken(token: any) {
-  try {
-    const url =
-      'https://oauth2.googleapis.com/token?' +
-      new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token: token.refreshToken,
-      });
+// async function refreshAccessToken(token: any) {
+//   try {
+//     const url =
+//       'https://oauth2.googleapis.com/token?' +
+//       new URLSearchParams({
+//         grant_type: 'refresh_token',
+//         refresh_token: token.refreshToken,
+//       });
 
-    const response = await fetch(url, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      method: 'POST',
-    });
+//     const response = await fetch(url, {
+//       headers: {
+//         'Content-Type': 'application/x-www-form-urlencoded',
+//       },
+//       method: 'POST',
+//     });
 
-    const refreshedTokens = await response.json();
+//     const refreshedTokens = await response.json();
 
-    if (!response.ok) {
-      throw refreshedTokens;
-    }
+//     if (!response.ok) {
+//       throw refreshedTokens;
+//     }
 
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
-    };
-  } catch (error) {
-    console.log(error);
+//     return {
+//       ...token,
+//       accessToken: refreshedTokens.access_token,
+//       accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+//       refreshToken: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+//     };
+//   } catch (error) {
+//     console.log(error);
 
-    return {
-      ...token,
-      error: 'RefreshAccessTokenError',
-    };
-  }
-}
+//     return {
+//       ...token,
+//       error: 'RefreshAccessTokenError',
+//     };
+//   }
+// }
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -87,18 +87,18 @@ export const authOptions: NextAuthOptions = {
       if (account && user) {
         return {
           accessToken: account.access_token,
-          accessTokenExpires: account.expires_at * 1000,
+          //accessTokenExpires: account.expires_at * 1000,
           refreshToken: account.refresh_token,
           user,
         };
       }
       // Return previous token if the access token has not expired yet
-      if (Date.now() < token.accessTokenExpires) {
-        return token;
-      }
+      // if (Date.now() < token.accessTokenExpires) {
+      //   return token;
+      // }
 
       // Access token has expired, try to update it
-      return refreshAccessToken(token);
+      return token;
     },
     redirect({ url, baseUrl }) {
       // Allows relative callback URLs
